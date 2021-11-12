b9f08e2ecb6c7d0bc957fa65f186d5198d5e3b58
NarcisoMB <ncismeba@gmail.com>
2021-11-12 17:03:58 -0600

    Detalles Generales

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,13 +1,3 @@
-$(".tiptext").mouseover(function() {
-    $(this).children(".description").show();
-}).mouseout(function() {
-    $(this).children(".description").hide();
-});
-
-$(document).bind("contextmenu",function(e) {
-    e.preventDefault();
-});
-
 var quickAccess = document.getElementById('quickAccess');
 var language = document.getElementById('language');
 var quickAccess = document.getElementById('quickAccess');
@@ -125,14 +115,38 @@ var modal = document.getElementById("myModal");
 var img = document.getElementById("fotoPerfil");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+var span = document.getElementsByClassName("close")[0];
+
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-var span = document.getElementsByClassName("close")[0];
-
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
